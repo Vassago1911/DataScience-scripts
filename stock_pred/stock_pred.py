e9e5464bb07df70ef86b97b7136d0258e35c1eb7
Marc Lange <marc@applike.info>
2018-01-02 08:13:44 +0100

    added forex-pred with EURCHF.csv

---
+++ stock_pred/stock_pred.py
@@ -0,0 +1,121 @@
+# Import
+import tensorflow as tf, numpy as np, pandas as pd, matplotlib.pyplot as plt
+from sklearn.preprocessing import MinMaxScaler
+
+# Import data         data = pd.read_csv('data_stocks.csv'); data = data.drop(['DATE'], 1); data.head()
+
+data = pd.read_csv('EURCHF.csv',header=None, usecols = [2,]);
+data.columns = ['open',]
+data = data[['open']]
+
+lookback=60; lookforward=10
+
+df = pd.concat([data.shift(-lookforward),]+[data.shift(i) for i in range(lookback)],axis=1).dropna()
+df.columns = ['open_target',]+['open_'+str(i) for i in range(lookback)]
+
+del data
+data = df.copy()
+del df 
+
+data_limit = 1000000
+data = data[-data_limit:]
+
+n = data.shape[0]; p = data.shape[1]; data = data.values
+print(n,p)
+
+# Training and test data
+train_start = 0; train_end = int(np.floor(0.8*n))
+test_start = train_end + 1; test_end = n
+data_train = data[np.arange(train_start, train_end), :]
+data_test = data[np.arange(test_start, test_end), :]
+
+# Scale data
+scaler = MinMaxScaler(feature_range=(-1, 1)); scaler.fit(data_train)
+data_train = scaler.transform(data_train); data_test = scaler.transform(data_test)
+
+# Build X and y
+X_train = data_train[:, 1:]; y_train = data_train[:, 0]
+X_test = data_test[:, 1:]; y_test = data_test[:, 0]
+
+#level-sizes
+levels = [X_train.shape[1], \
+512, 256, 128, 1]
+
+# Session
+net = tf.InteractiveSession()
+
+# Placeholder
+X = tf.placeholder(dtype=tf.float32, shape=[None, levels[0]])
+Y = tf.placeholder(dtype=tf.float32, shape=[None])
+
+# Initializers
+sigma = 1
+weight_initializer = tf.variance_scaling_initializer(mode="fan_avg", distribution="uniform", scale=sigma)
+
+# Hidden layer
+actn_fn = tf.nn.relu
+hidden_1 = tf.layers.dense(inputs=X, units=levels[1], activation=actn_fn, kernel_initializer = weight_initializer)
+hidden_2 = tf.layers.dense(inputs=hidden_1, units=levels[2], activation=actn_fn, kernel_initializer = weight_initializer)
+hidden_3 = tf.layers.dense(inputs=hidden_2, units=levels[3], activation=actn_fn, kernel_initializer = weight_initializer)
+#hidden_4 = tf.layers.dense(inputs=hidden_3, units=levels[4], activation=actn_fn, kernel_initializer = weight_initializer)
+out      = tf.transpose(tf.layers.dense(inputs=hidden_3, units=levels[4], activation=actn_fn, kernel_initializer = weight_initializer))
+#hidden_1 = actn_fn(tf.add(tf.matmul(X, W_hidden_1), bias_hidden_1))
+#hidden_2 = actn_fn(tf.add(tf.matmul(hidden_1, W_hidden_2), bias_hidden_2))
+#hidden_3 = actn_fn(tf.add(tf.matmul(hidden_2, W_hidden_3), bias_hidden_3))
+#hidden_4 = actn_fn(tf.add(tf.matmul(hidden_3, W_hidden_4), bias_hidden_4))
+
+# Output layer (transpose!)
+#out = tf.transpose(tf.add(tf.matmul(hidden_4, W_out), bias_out))
+
+# Cost function
+mse = tf.reduce_mean(tf.squared_difference(out, Y))
+
+# Optimizer
+opt = tf.train.AdamOptimizer().minimize(mse)
+
+# Init
+net.run(tf.global_variables_initializer())
+
+# Setup plot
+fig = plt.figure()
+ax1 = fig.add_subplot(111)
+
+# Fit neural net
+batch_size = 1024
+mse_train = []
+mse_test = []
+
+# Run
+epochs = 3
+for e in range(epochs):
+
+    # Shuffle training data
+    shuffle_indices = np.random.permutation(np.arange(len(y_train)))
+    X_train = X_train[shuffle_indices]
+    y_train = y_train[shuffle_indices]
+
+    # Minibatch training
+    for i in range(0, len(y_train) // batch_size):
+        start = i * batch_size
+        batch_x = X_train[start:start + batch_size]
+        batch_y = y_train[start:start + batch_size]
+        # Run optimizer with batch
+        net.run(opt, feed_dict={X: batch_x, Y: batch_y})
+
+        # Show progress
+        if np.mod(i, 50) == 0:
+            # MSE train and test
+            mse_train.append(net.run(mse, feed_dict={X: X_train, Y: y_train}))
+            mse_test.append(net.run(mse, feed_dict={X: X_test, Y: y_test}))
+            print('MSE Train: ', mse_train[-1])
+            print('MSE Test: ', mse_test[-1])
+            # Prediction
+            #pred = net.run(out, feed_dict={X: X_test})
+            #line2.set_ydata(pred)
+            #plt.title('Epoch ' + str(e) + ', Batch ' + str(i))
+            #plt.pause(0.01)
+
+pred = net.run(out, feed_dict={X: X_test})
+line1, = ax1.plot(y_test)
+line2, = ax1.plot(pred)
+plt.show()
